c0378cda59b9f7d6af6f8eb33bbe000ee5aeb31c
Alex Neslusan <deadfoxygrandpa@gmail.com>
2016-02-29 17:56:43 +0800

    Update for Elm 0.16

--- chapter2.elm
+++ chapter2.elm
@@ -1,5 +1,8 @@
-import Automaton (..)
-import Dict (..)
+module Main (..) where
+
+import Dict exposing (..)
+import Html
+
 
 {-
 Agents as described in chapter 2 of Russel and Norvig's AI book.
@@ -7,93 +10,180 @@ Goal is to design different agents that efficiently clean an area
 of 2 tiles (A and B). The possible actions are to suck or move to
 the left or right.
 -}
-
 -- An AI agent is really just a type of mapping from Percepts to Actions.
--- I will use an Automaton to represent them
-type Agent = Automaton Percept Action
+
+
+type alias Agent =
+  { agent | run : Percept -> Action }
+
+
 
 -- Possible actions the robotic vacuum cleaner can take: suck, move left,
 -- move right, or wait
-data Action = Suck | MoveRight | MoveLeft | Rest
+
+
+type Action
+  = Suck
+  | MoveRight
+  | MoveLeft
+  | Rest
+
+
 
 -- A perception of the current environment
-type Percept = (Location, Status)
-data Location = A | B
-data Status = Clean | Dirty
+
+
+type alias Percept =
+  ( Location, Status )
+
+
+type Location
+  = A
+  | B
+
+
+type Status
+  = Clean
+  | Dirty
+
+
 
 -- A lookup table mapping percepts to actions. Since Elm only supports Dicts
 -- with built-in types for the keys, I convert the values to Strings using
 -- the show function
-type Table = Dict String Action
+
+
+type alias Table =
+  Dict String Action
+
+
 
 -- A rule is a function that maps a percept to True or False, accompanied by
 -- an action to take if the function evaluates to True
-type Rule = ((Percept -> Bool), Action)
+
+
+type alias Rule =
+  ( Percept -> Bool, Action )
+
+
 
 -- An AI agent based on a lookup table. Adds the current percept to all
 -- previously encountered percepts, for a simple memory system
+
+
 tableDrivenAgent : Table -> Agent
-tableDrivenAgent table =
-    hiddenState (table, []) (\percept (table', percepts) ->
-        ((table', percept :: percepts), lookup (percept :: percepts) table'))
+tableDrivenAgent table percept =
+  Dict.get (Debug.log "percept" <| toString percept) table
+    |> Maybe.withDefault Rest
 
-lookup : [Percept] -> Table -> Action
-lookup percepts table = maybe Rest id <| get (show percepts) table
 
-tableVacuumAgent = tableDrivenAgent <| fromList [ ("[(A,Clean)]", MoveRight)
-                                                , ("[(A,Dirty)]", Suck)
-                                                , ("[(B,Clean)]", MoveLeft)
-                                                , ("[(B,Dirty)]", Suck)
-                                                , ("[(A,Clean),(A,Clean)]", MoveRight)
-                                                , ("[(A,Dirty),(A,Clean)]", Suck)
-                                                , ("[(A,Clean),(A,Clean),(A,Clean)]", MoveRight)
-                                                , ("[(A,Dirty),(A,Clean),(A,Clean)]", Suck)
-                                                ]
+tableVacuumAgent : Agent
+tableVacuumAgent =
+  tableDrivenAgent
+    <| Dict.fromList
+        [ ( "[(A,Clean)]", MoveRight )
+        , ( "[(A,Dirty)]", Suck )
+        , ( "[(B,Clean)]", MoveLeft )
+        , ( "[(B,Dirty)]", Suck )
+        , ( "[(A,Clean),(A,Clean)]", MoveRight )
+        , ( "[(A,Dirty),(A,Clean)]", Suck )
+        , ( "[(A,Clean),(A,Clean),(A,Clean)]", MoveRight )
+        , ( "[(A,Dirty),(A,Clean),(A,Clean)]", Suck )
+        ]
+
+
 
 -- A simple reflex agent with no memory. Just acts based on current status
+
+
 reflexVacuumAgent : Agent
-reflexVacuumAgent =
-    pure (\(location, status) ->
-        if (status == Dirty) then Suck
-        else case location of
-            A -> MoveRight
-            B -> MoveLeft)
+reflexVacuumAgent ( location, status ) =
+  if (status == Dirty) then
+    Suck
+  else
+    case location of
+      A ->
+        MoveRight
+
+      B ->
+        MoveLeft
+
+
 
 -- A generalized simple reflex agent. This could be used to define arbitrary agents
 -- that are not bound to the vacuum world problem. This could be done in a more
 -- succinct way, but I'm trying to be faithful to the models described in the
 -- book. As such, I defined 3 inner helper functions, but only ruleMatch is interesting
-simpleReflexAgent : [Rule] -> Agent
-simpleReflexAgent rules =
-    pure (\percept ->
-        let interperetInput (l, s) = (l, s)
-            ruleMatch state rules' = if (length rules' == 0) then ((\_ -> False), Rest)
-                                     else let (r::rs) = rules'
-                                              (f, a)  = r in
-                                          if   (f state)    then ((\_ -> True), a)
-                                          else ruleMatch state rs
-            ruleAction rule        = snd rule
-            state                  = interperetInput percept
-            rule                   = ruleMatch state rules in
-        ruleAction rule)
+
+
+simpleReflexAgent : List Rule -> Agent
+simpleReflexAgent rules percept =
+  let
+    interperetInput ( location, status ) =
+      ( location, status )
+
+    ruleMatch state rules =
+      case ( List.head rules, List.tail rules ) of
+        ( Just ( f, a ), Just rs ) ->
+          if f state then
+            ( (\_ -> True), a )
+          else
+            ruleMatch state rs
+
+        _ ->
+          ( (\_ -> False), Rest )
+
+    ruleAction rule =
+      snd rule
+
+    state =
+      interperetInput percept
+
+    rule =
+      ruleMatch state rules
+  in
+    ruleAction rule
+
+
 
 -- A reimplementation of the reflex vacuum agent based on the generalized simple
 -- reflex agent template
+
+
 reflexVacuumAgent2 : Agent
 reflexVacuumAgent2 =
-    let rules = [ ((\(location, status) -> status == Dirty), Suck)
-                , ((\(location, status) -> location == A), MoveRight)
-                , ((\(location, status) -> location == B), MoveLeft)
-                ] in
+  let
+    rules =
+      [ ( (\( location, status ) -> status == Dirty), Suck )
+      , ( (\( location, status ) -> location == A), MoveRight )
+      , ( (\( location, status ) -> location == B), MoveLeft )
+      ]
+  in
     simpleReflexAgent rules
+
+
 
 -- A function to run a simple test based on 2 successive inputs to an agent and
 -- output the results
-testAgent : Agent -> Element
-testAgent agent = let x = (step (A, Clean) agent) in
-                  asText <| (snd x, snd <| step (A, Dirty) (fst x))
 
-main = flow down [ plainText "tableVacuumAgent: " `beside` (testAgent tableVacuumAgent)
-                 , plainText "reflexVacuumAgent: " `beside` (testAgent reflexVacuumAgent)
-                 , plainText "reflexVacuumAgent2: " `beside` (testAgent reflexVacuumAgent2)
-                 ]
+
+testAgent : Agent -> Html.Html
+testAgent agent =
+  let
+    x =
+      agent ( A, Clean )
+
+    y =
+      agent ( B, Clean )
+  in
+    toString x |> Html.text
+
+
+main : Html.Html
+main =
+  Html.div
+    []
+    [ Html.div [] [ Html.text "tableVacuumAgent: ", (testAgent tableVacuumAgent) ]
+    , Html.div [] [ Html.text "reflexVacuumAgent: ", (testAgent reflexVacuumAgent) ]
+    , Html.div [] [ Html.text "reflexVacuumAgent2: ", (testAgent reflexVacuumAgent2) ]
+    ]
